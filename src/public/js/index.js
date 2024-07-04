024d21d962d381f7a2a7d5632e105d8852a1ccaa
bossin <scorpinosred@gmail.com>
2024-07-04 19:08:04 -0400

    update en el reCAPTCHA

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,21 +1,18 @@
                                      
-  
+  // ir a la pagina > google.com/recaptcha/about/
+  // acceder a V3 Admin Console  (hacer esto con la cuenta de google scorpinosred@gmail.com)
 
  /*  reCaptchat  */
 
-  function onSubmit(token) {
+  function forgottenpassw(token) { 
       let response = grecaptcha.getResponse();
-      document.getElementById("forgotten").submit();
+      document.getElementById("forgotten").submit(); //este id es el id del formulario, cuando reciba la respuesta de google recatchar este envia el formulario.
   }
-
-
 
   function onSubmitSignin(token) {
       let response = grecaptcha.getResponse();
       document.getElementById("signin").submit();
   }
-
-
 
   function onSubmitSignup(token) {
       let response = grecaptcha.getResponse();
