eac266c6d14c62852aa849ea888584a4cb017ba1
ghezat <unitradingpro@gmail.com>
2025-09-30 17:25:48 -0400

    add google-auth20 3

--- src/server.js
+++ src/server.js
@@ -55,7 +55,8 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:1263/auth/google/callback"
+    //callbackURL: "http://localhost:1263/auth/google/callback"
+    callbackURL: "https://blissenet.com/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
 
     return done(null, profile);
