5dda7ff8e99fcc68f2588fb3a155e6d1c079675a
ghezat <unitradingpro@gmail.com>
2026-02-23 09:26:22 -0400

    new code info of telegram v12

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -5450,7 +5450,7 @@ routes.post(`/webhook/${Token}`, async(req, res) => {
             // Verificar si updateUser es válido
             if (updateUser !== null && updateUser !== undefined) {
                 // Enviar mensaje de bienvenida al usuario
-                const Message = `¡Hola! ${userBliss} ha sincronizado satisfactoriamente con BlissBot, ahora podrás recibir todas las notificaciones en tu Telegram`;
+                const Message = `¡Hola! ${userBliss} cuenta sincronizada satisfactoriamente con BlissBot, ahora podrás recibir todas las notificaciones en tu Telegram.\n 📍Obtener información use el comando /info `;
 
                 axios.post(`https://api.telegram.org/bot${Token}/sendMessage`, {
                     chat_id: chatId,
